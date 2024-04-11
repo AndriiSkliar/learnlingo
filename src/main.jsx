ce65c0e7a81edd0fcc48389c0160f93d80e2cc77
Андрей Скляр <andreas.skliar@gmail.com>
2024-04-11 23:33:52 +0300

    fixed/store name

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import { store } from './redux/store.js';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
 import './index.css';
