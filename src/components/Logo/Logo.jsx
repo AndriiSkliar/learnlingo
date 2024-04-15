ab7569f0561578d646d7b53aff5add53897554ed
Андрей Скляр <andreas.skliar@gmail.com>
2024-04-15 13:12:29 +0300

    coding with forms

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Logo = () => {
+  return (
+    <>
+      <Link to="/home">
+        <p>LearnLingo</p>
+      </Link>
+    </>
+  );
+};
+
+export default Logo;
