edcfe6cee354e7a5d32415ec195d6abbe4d64102
Андрей Скляр <andreas.skliar@gmail.com>
2024-04-19 12:15:01 +0300

    rename file

---
+++ src/components/LoadData.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect } from 'react';
+import { ref, query, onValue, orderByKey, startAt, endBefore } from 'firebase/database';
+import { db } from '../../firebase';
+import { TeachersCard } from './TeachersCard';
+import { nanoid } from 'nanoid';
+
+export const LoadData = () => {
+  const [loadedData, setLoadedData] = useState([]);
+  const [currentOffset, setCurrentOffset] = useState(4);
+  const [noMoreData, setNoMoreData] = useState(true);
+  const dataLength = loadedData.length;
+
+  useEffect(() => {
+    loadDataFromFB();
+  }, []);
+  
+  const loadDataFromFB = async () => {
+    const baseFetch = query(ref(db), orderByKey(), endBefore(String(currentOffset)));
+    const updFetch = query(ref(db), orderByKey(), startAt(String(currentOffset)), endBefore(String(currentOffset + 4)));
+    const queryRef = dataLength > 0 ? updFetch : baseFetch;
+    
+    try {
+      onValue(queryRef, (snapshot) => {
+        const newData = [];
+        snapshot.forEach((childSnapshot) => {
+          newData.push(childSnapshot.val());
+        });
+        setLoadedData([...loadedData, ...newData]);
+        setCurrentOffset(dataLength + 4);
+
+        if (newData.length < 4 && dataLength > 0) setNoMoreData(false);
+      });
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div>
+      {dataLength > 0 &&
+        <ul>
+          {loadedData.map((card) => (
+            <TeachersCard key={nanoid()} card={card}
+          />))}
+        </ul>}
+      {noMoreData && <button onClick={loadDataFromFB}>Load more</button>}
+    </div>
+  );
+};
