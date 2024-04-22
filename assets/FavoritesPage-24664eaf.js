7982b73c12cf8648720d09cca070a551ac78ee9d
AndriiSkliar <89527037+AndriiSkliar@users.noreply.github.com>
2024-04-22 23:35:33 +0000

    Deploying to gh-pages from @ AndriiSkliar/learnlingo@57c14a7cb688445573a48da5f01499838310fa70 🚀

---
+++ assets/FavoritesPage-24664eaf.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-49427db5.js";const t=()=>e.jsx("div",{children:"FavoritesPage"});export{t as default};
---
+++ assets/HomePage-f80b86af.js
@@ -0,0 +1,1 @@
+import{j as e,L as s}from"./index-49427db5.js";const t=()=>e.jsxs(e.Fragment,{children:[e.jsx("p",{children:"Welcome!"}),e.jsxs("p",{children:["Go to the ",e.jsx(s,{to:"/teachers",children:"teachers"})]})]});export{t as default};
